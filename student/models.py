6bd6ce487a75f0502c370c45ea7cb2dfcdca633c
Arifen <113052888+ArifenFahim@users.noreply.github.com>
2025-06-18 02:01:30 +0600

    Creating Media folder based on Student

--- student/models.py
+++ student/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+import os
 
+def student_directory_name(instance,filename):
+    return os.path.join('student/media', instance.name, filename)
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=200)
@@ -7,7 +10,7 @@ class Student(models.Model):
     phone = models.CharField(max_length=50)
     password = models.CharField(max_length=150)
     checkbox = models.BooleanField()
-    photo = models.ImageField(upload_to='photos/',default=None,null=True)
+    photo = models.ImageField(upload_to=student_directory_name,default=None,null=True)
     
     def __str__(self):
         return f"{self.name}"
