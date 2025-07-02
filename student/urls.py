c1a48755918e3aff207edb1a7b38d7b617559e79
Arifen <113052888+ArifenFahim@users.noreply.github.com>
2025-07-03 01:11:48 +0600

    Class based views

--- student/urls.py
+++ student/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from .import views
 urlpatterns = [
-    path('home/',views.home,name="home"),
-    path('create/',views.create_student, name="create_student"),
-    path('edit/<int:id>',views.update_student, name="update_student"),
-    path('delete/<int:id>',views.delete_student, name="delete_student"),
+    # path('home/',views.home,name="home"), #function based
+    path('home/',views.StudentLists.as_view(),name="home"),#class based views
+    # path('create/',views.create_student, name="create_student"),  #function based
+    path('create/',views.CreatrStudent.as_view(), name="create_student"),  #class based view
+    # path('edit/<int:id>',views.update_student, name="update_student"), #function based
+    path('edit/<int:id>',views.updateStudentData.as_view(), name="update_student"), #class based view
+    # path('delete/<int:id>',views.delete_student, name="delete_student"), #function based
+    path('delete/<int:id>',views.DeleteStudentData.as_view(), name="delete_student"), #class based view
 ]
--- student/views.py
+++ student/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render , HttpResponse , redirect
 from .import models
 from . import forms
 from django.contrib import messages
+from django.views.generic import ListView , UpdateView ,DeleteView ,CreateView
+from django.urls import reverse_lazy
 #There are three types of forms in Django
 '''
     1.HTML form
@@ -46,9 +48,31 @@ def create_student(request):
         form = forms.StudentForm()
     return render(request,'student/create_student.html', {'form':form})
 
+class CreatrStudent(CreateView):
+    
+    form_class = forms.StudentForm
+    success_url = reverse_lazy('home')
+    template_name = 'student/create_student.html'
+    def form_valid(self, form):
+        messages.add_message(self.request, messages.SUCCESS, 'Student Created Successfully.')
+        return super().form_valid(form)
+
+
 def home(request):
     students = models.Student.objects.all()
     return render(request,'student/index.html', {'students':students})
+
+
+#for class base views
+class StudentLists(ListView):
+    model = models.Student
+    template_name = 'student/index.html'
+    context_object_name = 'students'
+
+
+
+
+
 
 def update_student(request,id):
     student = models.Student.objects.get(id=id)
@@ -62,8 +86,36 @@ def update_student(request,id):
             return redirect('home')
     return render(request,'student/create_student.html', {'form':form, 'edit': True})
 
+#update student data
+class updateStudentData(UpdateView):
+    form_class = forms.StudentForm
+    model = models.Student
+    template_name = 'student/create_student.html'
+    success_url = reverse_lazy('home')
+    pk_url_kwarg = 'id'
+    def form_valid(self, form):
+        """If the form is valid, save the associated model."""
+        messages.add_message(self.request, messages.SUCCESS, 'Student updated Successfully.')
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(). get_context_data(**kwargs)
+        context['edit'] = True
+        return context
+
+
 def delete_student(request,id):
     student = models.Student.objects.get(id=id) #id wala student k khujy pelam and object o pelam
     student.delete() #oi student k delete korlam
     messages.add_message(request, messages.SUCCESS, 'Student delete Successfully.')
     return redirect('home') #success holy take home page a redirect korlam
+
+class DeleteStudentData(DeleteView):
+    model = models.Student
+    pk_url_kwarg = 'id'
+    success_url = reverse_lazy('home')
+    template_name = 'student/delete_student.html'
+    def delete(self, request, *args, **kwargs):
+        messages.add_message(request, messages.SUCCESS, 'Student delete Successfully.')
+        return super().delete(request, *args, **kwargs) 
+        
